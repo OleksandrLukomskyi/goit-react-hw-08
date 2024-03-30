6690bcf4d4624e2a57658df843ee17aa1e3fd986
OleksandrLukomskyi <121460896+OleksandrLukomskyi@users.noreply.github.com>
2024-03-30 18:58:44 +0100

    Update operations.js

--- src/store/auth/operations.js
+++ src/store/auth/operations.js
@@ -54,9 +54,17 @@ export const refreshUser = createAsyncThunk(
     try {
       setAuthHeader(persistedToken);
       const res = await axios.get("/users/current");
+
       return res.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
     }
+  },
+  {
+    condition: (_, { getState }) => {
+      const reduxState = getState();
+      const savedToken = reduxState.auth.token;
+      return savedToken !== null;
+    },
   }
 );
